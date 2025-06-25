5d5b07e2670db1ec7cb87d4f2c08fa1a5530e133
eDanSaMa <danielsalasmtz11@gmail.com>
2025-06-25 13:35:27 -0600

    Pa deploy z

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, './dist/golden-treasure/browser')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/golden-treasure/browser/index.html'));
-});
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', routes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/golden-treasure/browser/index.html'));
+});
 
 const PORT = process.env.PORT;
 
