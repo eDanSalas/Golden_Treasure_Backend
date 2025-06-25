bc9babbfd01f042fb4cb0b81f2149e0001110497
eDanSaMa <danielsalasmtz11@gmail.com>
2025-06-25 17:49:51 -0600

    Epicooo

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', routes);
 
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, './dist/golden-treasure/browser/index.html'));
+});
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
-
-app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, './dist/golden-treasure/browser/index.html'));
 });
