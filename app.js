c7e6cbb98f7aacb13e96dd551d91f2c884872f9f
eDanSaMa <danielsalasmtz11@gmail.com>
2025-06-24 20:58:42 -0600

    Backend Final

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, './dist/golden-treasure/browser')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/golden-treasure/browser/index.html'));
-});
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', routes);
